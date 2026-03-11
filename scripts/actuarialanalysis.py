5f6e6d96a6b8c5b12cc171a5b24603637ed7f447
osalgador <oscareduardo_salgado@hotmail.com>
2026-03-10 18:23:55 -0600

    Fix typos and improve comments in actuarialanalysis.py

--- scripts/actuarialanalysis.py
+++ scripts/actuarialanalysis.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
-#i11mportramos nuestros datos como dataframes
+#importramos nuestros datos como dataframes
 dfcarIns= pd.read_csv("Insurance claims data.csv")#DATAFRAME VEHICLE RISK ANALYSIS
 dfLife=pd.read_csv("Insurance_claims.csv")#DATAFRAME LIFE & FRAUD ANALYSIS
 dfcarIns.shape
 dfcarIns.info()
 dfcarIns.head()
-#Elimanremos datos vacíos y datos que no aporten al modelo
+#Eliminaremos datos vacíos y datos que no aporten al modelo
 dfcarIns.isnull().sum()
 dfcarIns=dfcarIns.drop(columns=["policy_id"])
 #mapearemos toda columna que tenga datos de si y no para volverlos binarios y usarlos en nuestro análisis
@@ -57,7 +57,9 @@ dfcarIns['max_torque'] = dfcarIns['max_torque'].str.extract('(\d+)').astype(floa
 dfcarIns['max_power'] = dfcarIns['max_power'].str.extract('(\d+)').astype(float)
 print(dfcarIns[['max_power','max_torque']])
 #Se creará nuevas variables para mejorar el modelo usado 
-dfcarIns['power_weight_ratio'] = dfcarIns['max_power'] / dfcarIns['gross_weight']#con esta variable veremos que tan potente es el vehículo conforme a su peso
-dfcarIns['young_driver'] = (dfcarIns['customer_age'] < 25).astype(int)#estadísticamente los jovenes menores de 25 tienden a tener más accidentes
+dfcarIns['power_weight_ratio'] = dfcarIns['max_power'] / dfcarIns['gross_weight']
+#con esta variable veremos que tan potente es el vehículo conforme a su peso
+dfcarIns['young_driver'] = (dfcarIns['customer_age'] < 25).astype(int)
+#estadísticamente los jovenes menores de 25 tienden a tener más accidentes
 #Estas dos variables permitirán analizar los conductores jóvenes y el número de los reclamos que existen entre ellos
 dfcarIns.to_csv("insurance_dashboard_dataset.csv", index=False)
